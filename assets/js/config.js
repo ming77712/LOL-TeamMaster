e48c61d29496e841ed18dd63804edacfd8f4519d
Ming <a955269@gmail.com>
2024-05-22 16:59:50 +0800

    edit config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,4 +2,4 @@ export const api_path = 'https://lol-teammaster.onrender.com';
 export const userIsLogin = localStorage.getItem('user');
 export const memberId = Number(localStorage.getItem('userId'));
 export const memberAvatar = localStorage.getItem('userAvatar');
-export const localhost = 'https://hsienlu.github.io';
+export const localhost = 'https://ming77712.github.io';
